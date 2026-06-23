49e09f05bfe06b50b2f52eadcdbb91ddb54f7ff7
ARSHINI SUPRITHA S <arshinisupritha.s@gmail.com>
2026-06-24 00:52:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,73 @@
+async function getWeather() {
+
+    const city =
+        document.getElementById("cityInput").value;
+
+    const result =
+        document.getElementById("weatherResult");
+
+    if(city===""){
+        result.innerHTML =
+        "Please enter a city name";
+        return;
+    }
+
+    try {
+
+        // Get City Coordinates
+
+        const geoResponse =
+        await fetch(
+        `https://geocoding-api.open-meteo.com/v1/search?name=${city}&count=1`
+        );
+
+        const geoData =
+        await geoResponse.json();
+
+        if(!geoData.results){
+            throw new Error("City not found");
+        }
+
+        const latitude =
+        geoData.results[0].latitude;
+
+        const longitude =
+        geoData.results[0].longitude;
+
+        // Get Weather
+
+        const weatherResponse =
+        await fetch(
+        `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,wind_speed_10m`
+        );
+
+        const weatherData =
+        await weatherResponse.json();
+
+        result.innerHTML = `
+        <h2>${city}</h2>
+
+        <p>
+        Temperature:
+        ${weatherData.current.temperature_2m} °C
+        </p>
+
+        <p>
+        Humidity:
+        ${weatherData.current.relative_humidity_2m} %
+        </p>
+
+        <p>
+        Wind Speed:
+        ${weatherData.current.wind_speed_10m} km/h
+        </p>
+        `;
+
+    }
+
+    catch(error){
+
+        result.innerHTML =
+        `<p>${error.message}</p>`;
+    }
+}
